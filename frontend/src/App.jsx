9d8c6aba0d80a13b434828c7e81c5b624861c161
Chirag-S-kotian <ckotian117@gmail.com>
2024-07-15 11:27:32 +0530

    "Refactored App component to import ContactList, removed empty JSX, and reformatted code"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,22 @@
-import {useState,useEffect} from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import ContactList from "./components/ContactList";
+import "./App.css";
 
 function App() {
-  const [contacts,setContacts] = useState([])
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
-    fetchContacts()
-  },[])
+    fetchContacts();
+  }, []);
 
   const fetchContacts = async () => {
-    const response = await fetch('http://127.0.0.1:5000/contacts')
-    const data = await response.json()
-    setContacts(data.contacts)
-    console.log(data,contacts)
-  }
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+    console.log(data, contacts);
+  };
 
-  return (
-    <>
-      
-    </>
-  )
+  return <ContactList contacts={contacts} />;
 }
 
-export default App
+export default App;
